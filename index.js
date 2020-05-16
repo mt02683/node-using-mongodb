f2ee786954d9e590648d60f1d8507ccb7129013c
Maggie Tsui <maggie02683@gmail.com>
2020-05-17 00:43:14 +0800

    Completed module

--- index.js
+++ index.js
@@ -15,3 +15,21 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+//Source: https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
+//Import the mongoose module
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/productsdb';
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
